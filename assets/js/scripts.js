1d822a26ef8b5286f4bf1cc545d6100c89fdb478
DavidPlociennikDev <nean12.bg@gmail.com>
2022-06-23 22:10:56 +0200

    frontend

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,26 @@
 function toogleMenu() {
     document.getElementById("menu").classList.toggle("show-menu");
 }
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dotsContainer: '#carousel-custom-dots',
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+})
+
+if (window.screen.width > 1350) {
+    $(document).ready(function () {
+        var valueLeft = (window.screen.width - 1350) / 2;
+        document.getElementById("carousel-custom-dots").style.left = valueLeft + 'px';
+    });
+    window.addEventListener('resize', function (event) {
+        var valueLeft = (window.screen.width - 1350) / 2;
+        document.getElementById("carousel-custom-dots").style.left = valueLeft + 'px';
+    }, true);
+
+}
